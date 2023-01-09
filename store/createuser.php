7c5270e3db8efc52e56c9bb8c622f702470c2b14
Ishini0818 <101697017+Ishini0818@users.noreply.github.com>
2023-01-09 12:35:44 +0530

    Update createuser.php

--- store/createuser.php
+++ store/createuser.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Create User</title>
-</head>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ONE SHOP</title>
+    <link rel="shortcut icon" href=".\Assets\icon.png" />
+    <link rel="stylesheet" href="Styles/style.css">
+  </head>
+
 <body class="home">
 	<?php
 	if($_POST){
