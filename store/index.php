9e79519f2370f7fa53270dd49fcdbab39747c86d
DilukChamika <103917677+DilukChamika@users.noreply.github.com>
2023-01-10 03:32:22 +0530

    Add files via upload

--- store/index.php
+++ store/index.php
@@ -11,8 +11,39 @@ if(!$_SESSION['auth']){
     $_SESSION['role']=$role;
 
     if($role== "admin"){
+        ?>
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>ONE SHOP</title>
+            <link rel="shortcut icon" href="Assets/icon.png" />
+        </head>
+       
+        </html>
+
+
+        <?php
+
         include("ContentSource/admin/adminStore.php");
     }else{
+        ?>
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>ONE SHOP</title>
+            <link rel="shortcut icon" href="Assets/icon.png" />
+        </head>
+     
+        </html>
+
+
+<?php
         include("ContentSource/user/user_store_page.php");
     }
     
