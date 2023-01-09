2ee26197f91f6aa801baf5f3937e1effb56fcc22
Ishini0818 <101697017+Ishini0818@users.noreply.github.com>
2023-01-09 12:36:46 +0530

    Update login.php

--- store/login.php
+++ store/login.php
@@ -48,6 +48,17 @@ if($_POST){
 ?>
 
 <html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ONE SHOP</title>
+  <link rel="shortcut icon" href=".\Assets\icon.png" />
+  <link rel="stylesheet" href="Styles/style.css">
+</head>
 <body class="home">
 
   <center>
